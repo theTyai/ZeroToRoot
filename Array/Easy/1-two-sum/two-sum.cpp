e12c83f29036e00c3e4852a5e566cec875b08e8e
theTyai <work.mishraashish@gmail.com>
2025-12-21 21:41:10 +0545

    Time: 0 ms (100.00%) | Memory: 14.9 MB (20.05%) - LeetSync

---
+++ Array/Easy/1-two-sum/two-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++)
+        {
+            int n=target-nums[i];
+            if(mp.count(n)) return {i,mp[n]};
+            mp[nums[i]]=i;
+        }
+        return {};
+    }
+};
