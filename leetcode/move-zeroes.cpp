43b7e9f0749efece45a5664bd979ba11284b2972
theTyai <work.mishraashish@gmail.com>
2025-12-21 23:23:24 +0545

    Add leetcode solution: move-zeroes

---
+++ leetcode/move-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        for(int j=i+1; j<nums.size(); j++)
+        {
+            if(nums[i]==0 && nums[j]!=0) swap(nums[i++],nums[j]);
+        }
+    }
+            else if(nums[i]!=0) i++;
+};
+
