d3fd28c91fc3f88552ab3784e281c053d72851b1
theTyai <work.mishraashish@gmail.com>
2025-12-23 17:14:28 +0530

    Add leetcode solution: minimum-size-subarray-sum

---
+++ leetcode/minimum-size-subarray-sum.cpp
@@ -0,0 +1,18 @@
+        while(r<nums.size()) {
+            }
+            s+=nums[r];
+                s-=nums[l]; 
+            while(s>=target) 
+                a=min(a, r-l+1);
+        return a==INT_MAX?0:a;
+                l++;
+        }
+        int l=0, r=0, s=0, a=INT_MAX;
+    int minSubArrayLen(int target, vector<int>& nums) {
+public:
+class Solution {
+    }
+            r++;
+            {
+
+
