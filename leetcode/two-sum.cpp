4fe11a3c52dfaaa746247e4cac8d82b419b06c0a
theTyai <work.mishraashish@gmail.com>
2025-12-26 00:19:26 +0530

    Add leetcode solution: two-sum

--- leetcode/two-sum.cpp
+++ leetcode/two-sum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,·‌int>·‌mp;
-·‌·‌·‌·‌·‌·‌·‌·‌for(int·‌i=0;·‌i<nums.size();·‌i++)
-·‌·‌·‌·‌·‌·‌·‌·‌{
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌int·‌n=target-nums[i];
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌if(mp.count(n))·‌return·‌{i,mp[n]};
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌mp[nums[i]]=i;
-·‌·‌·‌·‌·‌·‌·‌·‌}
-·‌·‌·‌·‌·‌·‌·‌·‌return·‌{};
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++)
+        {
+            int n=target-nums[i];
+            if(mp.count(n)) return {i,mp[n]};
+            mp[nums[i]]=i;
+        }
+        return {};
     }
 };
 
