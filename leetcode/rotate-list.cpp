b7af7e3415f732132e3e963b059c985bc39bf562
theTyai <work.mishraashish@gmail.com>
2025-12-29 22:17:12 +0530

    Add leetcode solution: rotate-list

---
+++ leetcode/rotate-list.cpp
@@ -0,0 +1,19 @@
+        ListNode* first = head;
+
+        temp->next = nullptr;   // break list
+        ListNode* second = temp->next;
+
+        }
+            temp = temp->next;
+        for (int i = 1; i < n - k; i++) {
+        temp = head;
+        // split at (n-k)
+
+        if (k == 0) return head;
+        k %= n;
+
+        }
+            temp = temp->next;
+            n++;
+        while (temp) {
+
