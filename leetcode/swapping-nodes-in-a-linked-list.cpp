1b2a0796bec84f78d7447c42a44c008d53cfff29
theTyai <work.mishraashish@gmail.com>
2025-12-30 19:22:28 +0530

    Add leetcode solution: swapping-nodes-in-a-linked-list

---
+++ leetcode/swapping-nodes-in-a-linked-list.cpp
@@ -0,0 +1,18 @@
+        ListNode* s1 = head;
+        for (int i = 1; i < k; i++) {
+            s1 = s1->next;
+        }
+            temp = temp->next;
+            t++;
+        while (temp) {
+        ListNode* temp = head;
+        int t = 0;
+        if (!head || !head->next) return head;
+    ListNode* swapNodes(ListNode* head, int k) {
+public:
+class Solution {
+ */
+ * };
+        }
+        ListNode* s2 = head;
+
