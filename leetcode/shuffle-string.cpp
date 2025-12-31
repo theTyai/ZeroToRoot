d91a20756f25779605e146e3cef83daa71ad9f26
theTyai <work.mishraashish@gmail.com>
2025-12-31 19:56:42 +0530

    Add leetcode solution: shuffle-string

---
+++ leetcode/shuffle-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string a(s.length(),' ');
+        for(int x:indices)
+        {
+        }
+    }
+        int i=0;
+            a[x]=s[i++];
+        return a;
+};
+
