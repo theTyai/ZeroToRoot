36935a1e2ccedb29cf359ce7f9d1ea97a37d6b0a
theTyai <work.mishraashish@gmail.com>
2025-12-31 17:50:08 +0530

    Add leetcode solution: string-to-integer-atoi

---
+++ leetcode/string-to-integer-atoi.cpp
@@ -0,0 +1,18 @@
+            }
+            else if (isdigit(c)) {
+                started = true;
+                t = t * 10 + (c - '0');
+                started = true;
+                sign = (c == '-') ? -1 : 1;
+            if (!started && (c == '-' || c == '+')) {
+
+            if (!started && c == ' ') continue;
+        for (char c : s) {
+
+        bool started = false;
+        int sign = 1;
+        long long t = 0;
+    int myAtoi(string s) {
+public:
+class Solution {
+
