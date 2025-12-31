0bb10f44e6d0d554ed6744d0b49986d38d8328ec
theTyai <work.mishraashish@gmail.com>
2025-12-31 18:21:08 +0530

    Add leetcode solution: check-if-all-as-appears-before-all-bs

---
+++ leetcode/check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        bool b=false;
+        for(char c:s)
+        {
+            if(b && c=='a') return false;;
+        }
+    }
+            if(c=='b') b=true;
+        return true;
+};
+
