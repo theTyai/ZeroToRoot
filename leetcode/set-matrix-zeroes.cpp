aa3121ecf7406004cb2c0f2c116a702cf6d17078
theTyai <work.mishraashish@gmail.com>
2026-01-01 18:40:03 +0530

    Add leetcode solution: set-matrix-zeroes

---
+++ leetcode/set-matrix-zeroes.cpp
@@ -0,0 +1,19 @@
+        for(auto p : s) {
+            int r = p.first;
+            int c = p.second;
+
+·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌//·‌row·‌zero
+            for(int j = 0; j < m; j++)
+                matrix[r][j] = 0;
+
+            // column zero
+        }
+            }
+                }
+                    s.insert({i, j});
+                if(matrix[i][j] == 0) {
+            for(int j = 0; j < m; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int i = 0; i < n; i++)
+                matrix[i][c] = 0;
+
