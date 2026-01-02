915c5319e49bb0c43b2124500e42cb5520bb495b
theTyai <work.mishraashish@gmail.com>
2026-01-03 00:56:36 +0530

    Add leetcode solution: trapping-rain-water

---
+++ leetcode/trapping-rain-water.cpp
@@ -0,0 +1,18 @@
+                    leftMax = height[l];
+                else
+                    water += leftMax - height[l];
+                l++;
+            } else {
+                if (height[r] >= rightMax)
+                if (height[l] >= leftMax)
+            if (height[l] <= height[r]) {
+
+        while (l < r) {
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+        int l = 0, r = n - 1;
+        int n = height.size();
+    int trap(vector<int>& height) {
+public:
+class Solution {
+
